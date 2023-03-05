2ed03f8b837df2b5b8a80e72aa5ed2de29f7ddda
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-03-06 00:37:24 +0100

    initial commit

--- src/List/sectionLists.js
+++ src/List/sectionLists.js
@@ -4,83 +4,83 @@ export const sectionLists = [
         image:"Best-Ever-Meatloaf.jpg",
         title:"Beef",
         shortDescription:"Best Ever Meatloaf",
-        url:"BeefMeat.js"
+        url:"../components/Section/BeefMeat.js"
     },
     {
         
         image:"Spatchcocked-Butter-Roasted-Lemon-And-Herb-Turkey-With-Gravy.jpg",
         title:"Roasted",
         shortDescription:"Spatchcocked Butter-Roasted Lemon and Herb Turkey with Gravy",
-        url:""
+        url:"../components/Section/Roasted.js"
     },
     {
         
         image:"Fish-air.jpg",
         title:"Fish",
         shortDescription:"Aunt Carol's Spinach and Fish Bake",
-        url:""
+        url:"../components/Section/Fish.js"
     },
     {
         
         image:"Beans-Rice.jpg",
         title:"Beans And Rice Recipes",
         shortDescription:"Authentic Louisiana Red Beans and Rice",
-        url:""
+        url:"../components/Section/BeansRice.js"
     },
     {
         
         image:"Desserts-cream.jpg",
         title:"Desserts",
         shortDescription:"The Best Cream Cheese King Cake",
-        url:""
+        url:"../components/Section/DessertsCake.js"
     },
     {
         
         image:"Tortilla.jpg",
         title:"Tortilla Soup Recipes",
         shortDescription:"Dump-and-Go Instant Pot Tortilla Soup",
-        url:""
+        url:"../components/Section/TortillaSoup.js"
     },
     {
         
         image:"Gumbo.jpg",
         title:"GUMBO",
         shortDescription:"GOOD New Orleans Creole Gumbo",
-        url:""
+        url:"../components/Section/Gumbo.js"
     },
     {
         
         image:"TILAPIA.jpg",
         title:"Tilapia",
         shortDescription:"Pan-Seared Tilapia",
-        url:""
+        url:"../components/Section/Tilapia.js"
     },
     {
         
         image:"salad.jpg",
         title:"Salad",
         shortDescription:"Hearts of Palm and Avocado Salad",
-        url:""
+        url:"../components/Section/Salads.js"
     },
     {
         
         image:"Coleslaw-Recipes.jpg",
         title:"Coleslaw Recipes",
         shortDescription:"Copycat KFC Coleslaw",
-        url:""
+        url:"../components/Section/Coleslaw.js"
     },
     {
         
         image:"NO-MAYO.jpg",
         title:"No Mayo",
         shortDescription:"HOT GERMAN POTATO SALAD",
-        url:""
+        url:"../components/Section/Nomayo.js"
     },
     {
         
         image:"Tomatopasta.jpg",
         title:"Pasta Salad",
         shortDescription:"Tomato Pasta And Salad Recipes",
-        url:""
+        url:"../components/Section/TomatoSalad.js"
     },
 ]
